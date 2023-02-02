056446bb81a20ef6edb4112f21852e0d9589bb9e
Sam Souik <me@samuelsouik.com>
2023-02-01 23:19:24 -0600

    changed(api prefix): added v1 to the api route prefix

--- apps/password-manager-api/src/main.ts
+++ apps/password-manager-api/src/main.ts
@@ -9,7 +9,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.setGlobalPrefix('api');
+    app.setGlobalPrefix('api/v1');
     await app.listen(3333);
 }
 
