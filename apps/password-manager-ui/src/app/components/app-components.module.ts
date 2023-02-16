ecdfb578bb082839f946e8f0bc98ccc05c7e97e8
Sam Souik <me@samuelsouik.com>
2023-02-16 00:46:37 -0600

    changed(login component): updated the login component with material ui

--- apps/password-manager-ui/src/app/components/app-components.module.ts
+++ apps/password-manager-ui/src/app/components/app-components.module.ts
@@ -13,7 +13,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -116,6 +116,9 @@ import AppComponents from './index';
         MatSortModule,
         MatTableModule,
     ],
+    providers: [
+        {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}}
+      ],
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppComponentsModule {}
