11fadd87da915d5c5d0daf68da390ae1908666ad
Sam Souik <me@samuelsouik.com>
2023-02-17 13:58:34 -0600

    added(password recovery page): created the initial skeleton of the password recovery page

--- apps/password-manager-ui/src/app/pages/auth/index.ts
+++ apps/password-manager-ui/src/app/pages/auth/index.ts
@@ -1,4 +1,5 @@
 import { LoginComponent } from './login/login.component';
+import { PasswordRecoveryComponent } from './password-recovery/password-recovery.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
-export default [LoginComponent, SignUpComponent];
+export default [LoginComponent, PasswordRecoveryComponent, SignUpComponent];
---
+++ apps/password-manager-ui/src/app/pages/auth/password-recovery/password-recovery.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'password-manager-password-recovery',
+    templateUrl: './password-recovery.component.html',
+})
+export class PasswordRecoveryComponent {}
